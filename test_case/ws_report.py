b0e4564032f55103579b249ced5c34917caecc65
leen0910 <16955414@qq.com>
2020-05-06 16:43:46 +0800

    V2.3 修改发布状态

--- test_case/ws_report.py
+++ test_case/ws_report.py
@@ -7,6 +7,8 @@ import json
 import signal
 from common import read_message
 from common import check_action as c
+import time
+
 def stop(signum, frame):
 
   global is_sigint_up
@@ -34,12 +36,26 @@ def ws_connect():
         # data_query_position=rm.get_data("31","query_position")
         while(is_sigint_up==False):
             if ws.connected:
-                print("服务：%s连接成功!"%url)
+                # print("服务：%s连接成功!"%url)
                 # t=ws.recv()
                 t=json.loads(ws.recv())
-                print(t)
-                # print("查询脚本行数位置。")
-                # c.checkAction(ws,data_query_position)
+                if t["action"]=="publish.status":
+                    print("机器状态：%s"%t["data"]["state_machine"])
+                    print("lua模块状态：%s"%t["data"]["lua_state"])
+                    print("motion模块状态：%s"%t["data"]["motion_state"])
+                    print("系统当前全局速度：%s"%t["data"]["global_vel"])
+                if t["action"]=="publish.motionInfo":
+                    print("末端世界坐标系的位置：%s"%t["data"]["cart_pose"])
+                if t["action"]=="publish.error":
+                    print("错误码：%s"%t["data"]["code"])
+                if t["action"]=="publish.io.state":
+                    print("本地IO输入：%s"%t["data"]["localIO"]["input"])
+                    print("本地IO输出：%s"%t["data"]["localIO"]["output"])
+                    print("CanIO输入：%s"%t["data"]["canIO"]["input"])
+                    print("CanIO输出：%s"%t["data"]["canIO"]["output"])
+                # else:
+                #     print(t)
+                time.sleep(5)
         ws.close()
     except Exception as e:
         print("websocket连接失败：%s"%e)
