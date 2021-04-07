cdcf31d59a1e33e0daf107b1b193a98e078f84cd
leen0910 <16955414@qq.com>
2021-04-07 08:59:53 +0800

    V2.5.1新需求 更改本地modbus配置，增加线圈输入输出。 （只是为了迁就示教器之前写好的脚本，修改没什么意义。）

--- test_case/test12_ModbusIO_robot.py
+++ test_case/test12_ModbusIO_robot.py
@@ -10,11 +10,14 @@ import json
 
 class websocket_request(unittest.TestCase):
     server_index=[0]
-    IO_type=["D"]
-    address1=[0,1,2,3,4,10,11,12,13,14,15,16,17,18,19,20,30,31,32,33,34,35,36,37,38,39,40,41,42,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,100,101,102,13,14,15,106,107]  # uint16
-    address2=[300,304,308,312,316,320,324,328,332,336,340,344,348,354,358,452,456,460,464,468,472,482,486,490,494,498,502,512,516,520,524,528,532,542,546,550,554,558,562,602,606,610,614,618,636,640,644,648,662,666,670,674,678,692,696,700,704,708]  #double
-    address3=[352,362,400,402,404,406,408,410,450,480,510,540,600,630,660,690]  #int
-
+    IO_type="D"
+    IO_type_X="X"
+    IO_type_Y="Y"
+    address1=[0,1,2,3,4,5,6,7]  # uint16
+    address2=[100,104,108,112,116,120,124,128,132,136,140,144,148,154,158,452,456,460,464,468,472,482,486,490,494,498,502,512,516,520,524,528,532,542,546,550,554,558,562,602,606,610,614,618,636,640,644,648,662,666,670,674,678,692,696,700,704,708]  #double
+    address3=[152,162,400,402,404,406,408,410,450,480,510,540,600,630,660,690]  #int
+    address_X=[0,1,2,3,4,5,6,100,101,102,103,104,105,106,107,108,109,110,111,112]
+    address_Y=[0,1,2,3,4,5,6,7,8,9,10,11,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117]
     value1=[4,0,12,35,15749,22233]
     value2=[0,1.2,35.25555,3.141592678521,55,96956]
     value3=[0,-15,-135565]
@@ -288,45 +291,80 @@ class websocket_request(unittest.TestCase):
         c.checkAction(url,data_logout)
 
 
+    def test04_read_modbus_input_coil(self):
+        """读取MODBUS/本机&其它设备 /读input_coil"""
+        rm=read_message.ReadMessage()
+        data_login=rm.get_data("登录设备","login_admin")
+        url=self.ws
+        print("step 1、管理员登录。")
+        c.checkAction(url,data_login)
+        time.sleep(1)
+
+        data_modbus_read=rm.get_data("读取MODBUS","io_read_modbus")
+        print("step 2、读取input_coil。")
 
 
+        server_index=self.server_index
+        add=self.address_X
+        type=self.IO_type_X
 
 
-    # def test05_read_modbus_register(self):
-    #     """读取MODBUS/本机&其它设备 /读寄存器"""
-    #     rm=read_message.ReadMessage()
-    #     data_login=rm.get_data("登录设备","login_admin")
-    #     url=self.ws
-    #     print("step 1、管理员登录。")
-    #     c.checkAction(url,data_login)
-    #     time.sleep(1)
-    #
-    #     data_modbus_read=rm.get_data("读取MODBUS","io_read_modbus")
-    #     print("step 2、读取寄存器。")
-    #
-    #     server_index=self.server_index
-    #     name_list=["follow_ref_X","follow_ref_Y","follow_ref_Z","follow_ref_A","follow_ref_B","follow_ref_C","follow_dir_X","follow_dir_Y","follow_dir_Z","follow_dir_A","follow_dir_B","follow_dir_C","follow_vel","error_time","start_length","end_length","type_id"]
-    #
-    #     data_dict=json.loads(data_modbus_read)
-    #     for index in server_index:
-    #         data_dict["data"]["server_index"]=index
-    #         print("modbus机器号：%s"%index )
-    #         for nick_name in name_list:
-    #             data_dict["data"]["nick_name"]=nick_name
-    #             print("读取的寄存器地址别名：%s"%nick_name)
-    #
-    #             data_modbus_read=json.dumps(data_dict)
-    #             t=c.checkAction(url,data_modbus_read)
-    #             if t["success"]==True:
-    #                 print("%s号机器的 %s 寄存器地址值读取为：%s。"%(index,nick_name,t["data"]["value"]))
-    #             else:
-    #                 print("%s号机器的%s 寄存器地址设置失败。"%(index,nick_name))
-    #             time.sleep(0.5)
-    #
-    #
-    #     data_logout=rm.get_data("退出登录","logout")
-    #     print("step 3、退出登录。")
-    #     c.checkAction(url,data_logout)
+        data_dict=json.loads(data_modbus_read)
+
+        for index in server_index:
+            data_dict["data"]["server_index"]=index
+            data_dict["data"]["IO_type"]="%s"%type
+            print("modbus机器号：%s"%index )
+            for address in add:
+                data_dict["data"]["address"]=address
+                print("读取input_coil索引地址：%s"%address)
+
+                data_modbus_read=json.dumps(data_dict)
+                t=c.checkAction(url,data_modbus_read)
+                if t["success"]==True:
+                    print("%s号机器的 %s input_coil地址值读取为：%s。"%(index,address,t["data"]["value"]))
+                else:
+                    print("%s号机器的%s input_coil地址值读取失败。"%(index,address))
+        data_logout=rm.get_data("退出登录","logout")
+        print("step 3、退出登录。")
+        c.checkAction(url,data_logout)
+
+    def test05_read_modbus_output_coil(self):
+        """读取MODBUS/本机&其它设备 /读output_coil"""
+        rm=read_message.ReadMessage()
+        data_login=rm.get_data("登录设备","login_admin")
+        url=self.ws
+        print("step 1、管理员登录。")
+        c.checkAction(url,data_login)
+        time.sleep(1)
+
+        data_modbus_read=rm.get_data("读取MODBUS","io_read_modbus")
+        print("step 2、读取output_coil。")
+
+        server_index=self.server_index
+        add=self.address_Y
+        type=self.IO_type_Y
+
+        data_dict=json.loads(data_modbus_read)
+        for index in server_index:
+            data_dict["data"]["server_index"]=index
+            data_dict["data"]["IO_type"]="%s"%type
+            print("modbus机器号：%s"%index )
+            for address in add:
+                data_dict["data"]["address"]=address
+                print("读取output_coil索引地址：%s"%address)
+
+                data_modbus_read=json.dumps(data_dict)
+                t=c.checkAction(url,data_modbus_read)
+                if t["success"]==True:
+                    print("%s号机器的 %s output_coil地址值读取为：%s。"%(index,address,t["data"]["value"]))
+                else:
+                    print("%s号机器的%s output_coil地址值读取失败。"%(index,address))
+        data_logout=rm.get_data("退出登录","logout")
+        print("step 3、退出登录。")
+        c.checkAction(url,data_logout)
+
+
 
     # def test06_modbus_disconnect(self):
     #     """断开index_server的modbus连接 """
@@ -362,13 +400,14 @@ class websocket_request(unittest.TestCase):
         self.ws.close()
 
 if __name__ == "__main__":
-    # unittest.main()
-    for i in range(1,2):
-        suite = unittest.TestSuite()
-        # suite.addTest(websocket_request('setUp'))
-        suite.addTest(websocket_request('test01_write_modbus_register_uint16'))
-        suite.addTest(websocket_request('test02_write_modbus_register_double'))
-        suite.addTest(websocket_request('test03_write_modbus_register_int'))
-
-        # suite.addTest(websocket_request('tearDown'))
-        unittest.TextTestRunner(verbosity=2).run(suite)
+    unittest.main()
+    # for i in range(1,10):
+    #     suite = unittest.TestSuite()
+    #     # suite.addTest(websocket_request('setUp'))
+    #     suite.addTest(websocket_request('test01_write_modbus_register_uint16'))
+    #     suite.addTest(websocket_request('test02_write_modbus_register_double'))
+    #     suite.addTest(websocket_request('test03_write_modbus_register_int'))
+    #     suite.addTest(websocket_request('test04_read_modbus_input_coil'))
+    #     suite.addTest(websocket_request('test05_read_modbus_output_coil'))
+    #     # suite.addTest(websocket_request('tearDown'))
+    #     unittest.TextTestRunner(verbosity=2).run(suite)
