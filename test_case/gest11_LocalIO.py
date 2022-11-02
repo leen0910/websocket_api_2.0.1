3949071ee7743640c360ec9d6ae498f0ae6e9159
leen0910 <16955414@qq.com>
2022-11-02 09:37:48 +0800

    V2.5: test

--- test_case/gest11_LocalIO.py
+++ test_case/gest11_LocalIO.py
@@ -32,7 +32,7 @@ class websocket_request(unittest.TestCase):
         url=self.ws
         print("step 1、管理员登录。")
         c.checkAction(url,data_login)
-        time.sleep(1)
+        time.sleep(0.3)
 
         data_io_read=rm.get_data("读取本地IO","io_read_local")
         print("step 2、读取io_input：")
@@ -63,7 +63,7 @@ class websocket_request(unittest.TestCase):
         url=self.ws
         print("step 1、管理员登录。")
         c.checkAction(url,data_login)
-        time.sleep(1)
+        time.sleep(0.3)
 
         data_io_set=rm.get_data("设置本地IO","io_write_local_off")
         print("step 2、设置io_output:")
@@ -80,7 +80,7 @@ class websocket_request(unittest.TestCase):
                 print("output_IO: %s状态设置为0。"%name)
             else:
                 print("output_IO: %s状态修改失败。"%name)
-            time.sleep(0.1)
+            time.sleep(0.01)
 
             data_dict_read["data"]["name"]=name
             print("读取output_io：%s"%name )
@@ -90,7 +90,7 @@ class websocket_request(unittest.TestCase):
                 print("output_io: %s 的state值为：%s。"%(name,t["data"]["state"]))
             else:
                 print("output_io：%s读取失败。"%name)
-            time.sleep(0.1)
+            # time.sleep(0.05)
 
         data_logout=rm.get_data("退出登录","logout")
         print("step 3、退出登录。")
@@ -103,7 +103,7 @@ class websocket_request(unittest.TestCase):
         url=self.ws
         print("step 1、管理员登录。")
         c.checkAction(url,data_login)
-        time.sleep(0.2)
+        time.sleep(0.3)
 
         data_io_set=rm.get_data("设置本地IO","io_write_local_on")
         print("step 2、设置output_IO为on:")
@@ -132,11 +132,95 @@ class websocket_request(unittest.TestCase):
                 print("output_IO: %s 的state值为：%s。"%(name,t["data"]["state"]))
             else:
                 print("output_IO：%s读取失败。"%name)
-            time.sleep(0.1)
+            # time.sleep(0.05)
 
         data_logout=rm.get_data("退出登录","logout")
         print("step 3、退出登录。")
         c.checkAction(url,data_logout)
+
+    def test04_write_io_off_on(self):
+        """3、设置本地output_IO on:[0-31] """
+        rm=read_message.ReadMessage()
+        data_login=rm.get_data("登录设备","login_admin")
+        url=self.ws
+        print("step 1、管理员登录。")
+        c.checkAction(url,data_login)
+        time.sleep(0.1)
+
+        data_io_set=rm.get_data("设置本地IO","io_write_local_on")
+        print("step 2、设置output_IO为on:")
+        io_list=["Y0","Y1","Y2","Y3","Y4","Y5","Y6","Y7","Y8","Y9","Y10","Y11","Y12","Y13","Y14","Y15","Y16","Y17","Y18","Y19","Y20","Y21","Y22","Y23","Y24","Y25","Y26","Y27","Y28","Y29","Y30","Y31"]
+        state_list=[0,1]
+        data_dict=json.loads(data_io_set)
+
+        data_io_read=rm.get_data("读取本地IO","io_read_local_Z0")
+        data_dict_read=json.loads(data_io_read)
+        for name in io_list:
+            for state in state_list:
+                data_dict["data"]["name"]=name
+                data_dict["data"]["state"]=state
+                print("修改output_IO：%s"%name )
+                data_io_set=json.dumps(data_dict)
+                t=c.checkAction(url,data_io_set)
+                if t["success"]==True:
+                    print("output_IO: %s状态设置: %s。"%(name,state))
+                else:
+                    print("output_IO: %s状态修改失败。"%name)
+                time.sleep(0.01)
+
+                # data_dict_read["data"]["name"]=name
+                # print("读取output_IO：%s"%name )
+                # data_io_read=json.dumps(data_dict_read)
+                # t=c.checkAction(url,data_io_read)
+                # if t["success"]==True and t["data"]["state"]==1:
+                #     print("output_IO: %s 的state值为：%s。"%(name,t["data"]["state"]))
+                # else:
+                #     print("output_IO：%s读取失败。"%name)
+                # time.sleep(0.05)
+
+        data_logout=rm.get_data("退出登录","logout")
+        print("step 3、退出登录。")
+        c.checkAction(url,data_logout)
+
+    def test05_write_io_all(self):
+        """3、设置本地output_IO all:[0-31] """
+        rm=read_message.ReadMessage()
+        data_login=rm.get_data("登录设备","login_admin")
+        url=self.ws
+        print("step 1、管理员登录。")
+        c.checkAction(url,data_login)
+        time.sleep(0.1)
+
+        data_io_set=rm.get_data("设置本地IO","io_write_local_all")
+        print("step 2、设置output_IO全部状态")
+
+        value_list=[0,4294967295,3804175089,2058836592,45613040,4294844403          ,0,4294967295]
+        data_dict=json.loads(data_io_set)
+
+        # data_io_read=rm.get_data("读取本地IO","io_read_local_Z0")
+        # data_dict_read=json.loads(data_io_read)
+
+        for value in value_list:
+            data_dict["data"]["value"]=value
+            print("打开全部output。" )
+            data_io_set=json.dumps(data_dict)
+            t=c.checkAction(url,data_io_set)
+            if t["success"]==True:
+                if value==0:
+                    print("output_IO全部关闭")
+                else:
+                    print("output_IO全部打开")
+            else:
+                print("output_IO全部状态修改失败。")
+            time.sleep(0.5)
+
+
+
+        data_logout=rm.get_data("退出登录","logout")
+        print("step 3、退出登录。")
+        c.checkAction(url,data_logout)
+
+
 
     # def test04_read_io_default(self):
     #     """4、读取default IO:default 不可修改 """
@@ -287,13 +371,17 @@ class websocket_request(unittest.TestCase):
 
 if __name__ == "__main__":
     # unittest.main()
-    for i in range(1,20):
+
+
+    for i in range(1,10000):
         suite = unittest.TestSuite()
         suite.addTest(websocket_request('setUp'))
-        suite.addTest(websocket_request('test01_read_io_input'))
+        # suite.addTest(websocket_request('test01_read_io_input'))
         suite.addTest(websocket_request('test02_write_io_output_off'))
         suite.addTest(websocket_request('test03_write_io_on'))
-    #     suite.addTest(websocket_request('test04_read_io_default'))
-    #     # suite.addTest(websocket_request('test05_write_io_default_off'))
-    #     # suite.addTest(websocket_request('test06_write_io_default_on'))
+        suite.addTest(websocket_request('test02_write_io_output_off'))
+        suite.addTest(websocket_request('test03_write_io_on'))
+        suite.addTest(websocket_request('test04_write_io_off_on'))
+        suite.addTest(websocket_request('test05_write_io_all'))
+        suite.addTest(websocket_request('tearDown'))
         unittest.TextTestRunner(verbosity=2).run(suite)
